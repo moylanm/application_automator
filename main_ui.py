a7a89d25237a82df48a07404c21251c9d1d1e918
Myles Moylan <moylanm@fastmail.com>
2024-10-19 11:08:45 -0700

    Begin form filling logic

--- main_ui.py
+++ main_ui.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python
-# -*- coding: latin-1 -*-
-from PySide6.QtCore import Qt, QRect
-from helpers import create_form_elements
-from PySide6.QtWidgets import QMainWindow, QPushButton
+# -*- coding: utf-8 -*-
+import json
+from PySide6.QtCore import QRect
+from PySide6.QtWidgets import QLabel, QLineEdit, QMainWindow, QPushButton
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from time import sleep
+
 
 class MainUI(QMainWindow):
 
@@ -11,23 +19,48 @@ class MainUI(QMainWindow):
         self.setup_window()
         self.init_ui()
 
+        with open('form_data.json') as f:
+            self.form_data = json.load(f)
+
     def setup_window(self):
-        self.setWindowTitle("Application Automator")
+        self.setWindowTitle('Application Automator')
         self.resize(600, 300)
         self.move(1000, 200)
 
     def init_ui(self):
-        create_form_elements(self)
-        self.create_button("Fill", QRect(15, 140, 75, 30), self.fill_application)
-        self.create_button("Submit", QRect(95, 140, 75, 30), self.submit_application)
+        self.url_label = QLabel('URL:', self)
+        self.url_label.setGeometry(QRect(15, 10, 60, 30))
+        self.url_field = QLineEdit(self)
+        self.url_field.setGeometry(QRect(15, 40, 400, 30))
+
+        self.captcha_label = QLabel('CAPTCHA:', self)
+        self.captcha_label.setGeometry(QRect(15, 75, 70, 30))
+        self.captcha_field = QLineEdit(self)
+        self.captcha_field.setGeometry(QRect(15, 105, 100, 30))
+
+        self.create_button('fill', QRect(15, 140, 75, 30), self.fill_application)
+        self.create_button('submit', QRect(95, 140, 75, 30), self.submit_application)
 
     def create_button(self, text, geometry, callback):
-        button = QPushButton(text, self)
+        button = QPushButton(text.title(), self)
+        button.setObjectName(text)
         button.setGeometry(geometry)
         button.clicked.connect(callback)
 
     def fill_application(self):
-        pass
+        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+        wait = WebDriverWait(driver, 5)
+
+        driver.maximize_window()
+        driver.get(self.url_field.text())
+
+        driver.find_element(By.XPATH, self.form_data['interested-button-xpath']).click()
+        wait.until(EC.element_to_be_clickable((By.XPATH, self.form_data['submit-button-xpath'])))
+
+        for item in self.form_data['basic-info']:
+            driver.find_element(By.XPATH, item['xpath']).send_keys(item['value'])
+
+        sleep(5)
 
     def submit_application(self):
         pass
