d22f59589c28d4cbdd607e93360048baec2f9880
Myles Moylan <moylanm@fastmail.com>
2024-10-19 06:22:31 -0700

    Stub out boilerplate code

---
+++ app.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+
+from main_ui import MainUI
+from PySide6.QtWidgets import QApplication
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    main = MainUI()
+    main.show()
+    sys.exit(app.exec())
---
+++ main_ui.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: latin-1 -*-
+from PySide6.QtCore import Qt, QRect
+from PySide6.QtWidgets import QMainWindow
+
+class MainUI(QMainWindow):
+
+    def __init__(self, parent=None):
+        super(MainUI, self).__init__(parent)
+
