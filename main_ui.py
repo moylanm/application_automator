a03e6da813f94cad2b154ec8fcfac35ea8d69910
Myles Moylan <moylanm@fastmail.com>
2024-10-19 07:07:44 -0700

    Adjust initial window size/location

--- main_ui.py
+++ main_ui.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: latin-1 -*-
 from PySide6.QtCore import Qt, QRect
+from helpers import create_form_fields
 from PySide6.QtWidgets import QMainWindow
 
 class MainUI(QMainWindow):
 
     def __init__(self, parent=None):
         super(MainUI, self).__init__(parent)
+        self.setup_window()
+        self.init_ui()
 
+    def setup_window(self):
+        self.setWindowTitle("Application Automator")
+        self.resize(600, 300)
+        self.move(1000, 200)
+
+    def init_ui(self):
+        create_form_fields(self)
