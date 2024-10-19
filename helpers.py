cbfdec79b631509740f099a89d64a71837228707
Myles Moylan <moylanm@fastmail.com>
2024-10-19 08:06:57 -0700

    Adjust form elements

--- helpers.py
+++ helpers.py
@@ -4,16 +4,16 @@ from PySide6.QtCore import QRect
 from PySide6.QtWidgets import QLabel, QLineEdit
 
 FORM_METADATA = [
-    {"label": "URL :", "label_geometry": QRect(15, 10, 60, 30), "field_type": QLineEdit, "field_geometry": QRect(70, 10, 690, 30), "field_name": "url_field", "attr": "url"},
-    {"label": "CAPTCHA :", "label_geometry": QRect(25, 50, 60, 30), "field_type": QLineEdit, "field_geometry": QRect(70, 50, 690, 30), "field_name": "captcha_field", "attr": "captcha"}
+    {"label": "URL:", "label_geometry": QRect(15, 10, 60, 30), "field_geometry": QRect(15, 40, 400, 30), "field_name": "url_field"},
+    {"label": "CAPTCHA:", "label_geometry": QRect(15, 75, 70, 30), "field_geometry": QRect(15, 105, 100, 30), "field_name": "captcha_field"}
 ]
 
-def create_form_fields(parent):
+def create_form_elements(parent):
     for el in FORM_METADATA:
         label = QLabel(el["label"], parent)
         label.setGeometry(el["label_geometry"])
 
-        field = el["field_type"](parent)
-        field.setGeometry(el["label_geometry"])
+        field = QLineEdit(parent)
+        field.setGeometry(el["field_geometry"])
 
         setattr(parent, el["field_name"], field)
